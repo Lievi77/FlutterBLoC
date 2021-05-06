c18462ee31d293d1e3d858d61dfc6bfe0f86d199
Lev <lguzm038@uottawa.ca>
2021-05-06 11:34:57 -0400

    Implemented blocTest for increment and decrement functions

--- flutter_bloc_testing/lib/cubit/counter_state.dart
+++ flutter_bloc_testing/lib/cubit/counter_state.dart
@@ -1,9 +1,12 @@
 part of 'counter_cubit.dart';
 
-class CounterState {
+//! Need to specify how we should compare
+class CounterState extends Equatable {
   int counterValue;
   bool wasIncremented;
   CounterState({@required this.counterValue, this.wasIncremented});
-}
 
-class CounterInitial extends CounterState {}
+  //! overrdie equatable.props method for comparison details
+  //! Return the properties we will use for comparisons
+  List<Object> get props => [this.counterValue, this.wasIncremented];
+}
--- flutter_bloc_testing/test/cubit/counter_cubit_test.dart
+++ flutter_bloc_testing/test/cubit/counter_cubit_test.dart
@@ -1,4 +1,5 @@
 //! MAKE SURE TO IMPORT THE TEST PACKAGE AND THE CUBIT/BLOC YOU WANT TO TEST
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc_testing/cubit/counter_cubit.dart';
 import 'package:test/test.dart';
 
@@ -28,9 +29,32 @@ main() {
     test(
         'The initial state for the CounterCubit is CounterCubit(counterValue: 0)',
         () {
-      //expect function
-
+      // ? Unit Testing returns a failed test but why?
+      //! Dart compares intances by memory reference.
+      //! Since two diff instances are stored in diff parts of the memory, they are
+      //! not equal in dart's view.
+      //* To override this behaviour, use the EQUATABLE package
       expect(counterCubit.state, CounterState(counterValue: 0));
     });
+
+    //* Time to test the cubit's functions
+    //! Remember to describe explicitly what the test checks for
+    blocTest(
+        'The Cubit should emit a CounterState(counterValue:1, wasIncremented: true) when cubit.increment is called',
+        //! Build method returns the instance of the cubit, to make it available for testing
+        build: () => counterCubit,
+        //! Act takes the cubit an applies the function/action to test
+        act: (cubit) => cubit.increment(),
+        //! Expect returns interable list. It contains the expected emitted states
+        expect: () => [CounterState(counterValue: 1, wasIncremented: true)]);
+
+    blocTest(
+        'The Cubit should emit a CounterState(counterValue:-1, wasIncremented: false) when cubit.increment is called',
+        //! Build method returns the instance of the cubit, to make it available for testing
+        build: () => counterCubit,
+        //! Act takes the cubit an applies the function/action to test
+        act: (cubit) => cubit.decrement(),
+        //! Expect returns interable list. It contains the expected emitted states
+        expect: () => [CounterState(counterValue: -1, wasIncremented: false)]);
   });
 }
