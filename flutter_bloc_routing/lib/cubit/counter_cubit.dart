a0bdcdc9d6de436c206ca7b52420905f083d7c0e
Lev <lguzm038@uottawa.ca>
2021-05-12 15:08:05 -0400

    Added routing example

---
+++ flutter_bloc_routing/lib/cubit/counter_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counterValue: 0));
+
+  //! To emit a state stream in a cubit, use emit()
+  void increment() => emit(
+      CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
+
+  // To access a the current state's properties use state.PROPERTY_TO_ACCESS
+  void decrement() => emit(CounterState(
+      counterValue: state.counterValue - 1, wasIncremented: false));
+}
